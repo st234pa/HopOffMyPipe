3adc61044b82f0fcd5659f3a4d2cdb0a1d1514b0
st234pa <syoon1@stuy.edu>
2016-12-15 15:22:09 -0500

    started pipe_networking

---
+++ pipe_networking.c
@@ -0,0 +1,11 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int server_handshake(int *fc) {
+  
+}
+
+int client_handshake(int *ts) {
+
+}
---
+++ pipe_networking.h
@@ -0,0 +1,3 @@
+int server_handshake(int *fc);
+
+int client_handshake(int *ts);
